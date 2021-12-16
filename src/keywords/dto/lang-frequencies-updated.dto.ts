086e2823d60c388de78ee00a70dc1fd47915ced2
hmaravlad <hmara.vladislav2@gmail.com>
2021-12-16 17:28:24 +0200

    add validation

--- src/keywords/dto/lang-frequencies-updated.dto.ts
+++ src/keywords/dto/lang-frequencies-updated.dto.ts
@@ -1,6 +1,10 @@
+import { IsString, ValidateNested } from 'class-validator';
 import { LanguageFrequency } from '../entities/lang-frequency.entity';
 
 export class LanguageFrequenciesUpdatedDto {
+  @IsString()
   keyword: string;
+
+  @ValidateNested({ each: true })
   data: LanguageFrequency[];
 }
--- src/keywords/dto/new-projects-updated.dto.ts
+++ src/keywords/dto/new-projects-updated.dto.ts
@@ -1,6 +1,10 @@
+import { IsString, ValidateNested } from 'class-validator';
 import { Project } from '../entities/project.entity';
 
 export class NewProjectsUpdatedDto {
+  @IsString()
   keyword: string;
+
+  @ValidateNested({ each: true })
   data: Project[];
 }
