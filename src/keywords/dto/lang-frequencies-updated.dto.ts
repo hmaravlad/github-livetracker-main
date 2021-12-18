c9c7610ec8775a3dde31c39220227855dc95a4d3
hmaravlad <hmara.vladislav2@gmail.com>
2021-12-18 18:41:03 +0200

    fix language frequencies validation

--- src/keywords/dto/lang-frequencies-updated.dto.ts
+++ src/keywords/dto/lang-frequencies-updated.dto.ts
@@ -1,10 +1,13 @@
-import { IsNumber, ValidateNested } from 'class-validator';
+import { Type } from 'class-transformer';
+import { IsArray, IsNumber, ValidateNested } from 'class-validator';
 import { LanguageFrequency } from '../entities/lang-frequency.entity';
 
 export class LanguageFrequenciesUpdatedDto {
   @IsNumber()
   keywordId: number;
 
+  @IsArray()
   @ValidateNested({ each: true })
+  @Type(() => LanguageFrequency)
   data: LanguageFrequency[];
 }
--- src/keywords/entities/lang-frequency.entity.ts
+++ src/keywords/entities/lang-frequency.entity.ts
@@ -1,4 +1,9 @@
-export interface LanguageFrequency {
+import { IsNumber, IsString } from 'class-validator';
+
+export class LanguageFrequency {
+  @IsString()
   language: string;
+
+  @IsNumber()
   frequency: number;
 }
